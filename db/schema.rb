276b81287c3d86c3815cbcb48f5c404d40a61fa3
kingsleytan <sengking.tan@gmail.com>
2016-09-27 15:06:36 +0800

    change Ticket model, 'branch_ID' -> 'service_ID'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160927065909) do
+ActiveRecord::Schema.define(version: 20160927070433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 20160927065909) do
 
   create_table "tickets", force: :cascade do |t|
     t.integer  "number"
-    t.integer  "branch_ID"
+    t.integer  "service_ID"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

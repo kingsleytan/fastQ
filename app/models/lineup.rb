27ce2f5d37a79d2cfb012db22cb1a1956c7e4658
kingsleytan <sengking.tan@gmail.com>
2016-10-05 15:13:39 +0800

    added scheduler using Gem Whenever, added 'service_id' to 'Lineup' model

--- app/models/lineup.rb
+++ app/models/lineup.rb
@@ -1,4 +1,8 @@
 class Lineup < ApplicationRecord
-  belongs_to :branch
+  belongs_to :service
   has_many :tickets
+
+  def self.add_one
+    self.create(currentticket: 1)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161004035153) do
+ActiveRecord::Schema.define(version: 20161005070436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20161004035153) do
     t.integer  "currentticket"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.integer  "service_id"
   end
 
   create_table "offices", force: :cascade do |t|

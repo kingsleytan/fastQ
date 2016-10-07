c37890b157d948f17ce1d21b68151984a3173c68
Nicholas4896 <nicholasyhs@gmail.com>
2016-10-07 17:31:45 +0800

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-n# This file is auto-generated from the current state of the database. Instead
+# This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
@@ -16,6 +16,18 @@ ActiveRecord::Schema.define(version: 20161007072444) do
   enable_extension "plpgsql"
 
   create_table "add_slug_to_branches", force: :cascade do |t|
+    t.string   "slug"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "add_slug_to_offices", force: :cascade do |t|
+    t.string   "slug"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "add_slug_to_services", force: :cascade do |t|
     t.string   "slug"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
